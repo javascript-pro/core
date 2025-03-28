4135a8339f6f46dc5e8d96400fb611590adaad00
Goldlabel pr0 <github@goldlabel.pro>
2025-03-28 16:02:54 +0100

    Improved address bar

--- ui/address-bar.tsx
+++ ui/address-bar.tsx
@@ -30,15 +30,19 @@ function Params() {
   ) : null;
 }
 
+function formatSegment(segment: string) {
+  return segment
+    .replace(/-/g, ' ')
+    .replace(/^\w/, (c) => c.toUpperCase());
+}
+
 export function AddressBar() {
   const pathname = usePathname();
-
-  // Split path and filter out empty segments
   const segments = pathname.split('/').filter(Boolean);
 
   return (
     <div className="flex items-center gap-x-2 p-3.5 lg:px-5 lg:py-3">
-      {/* <div className="text-gray-600">
+      <div className="text-gray-600">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-4"
@@ -51,32 +55,34 @@ export function AddressBar() {
             clipRule="evenodd"
           />
         </svg>
-      </div> */}
+      </div>
 
       <div className="flex gap-x-1 text-sm font-medium items-center">
-        {pathname !== '/' ? (
-          <Link href="/" className="px-2 text-blue-600 hover:underline">
-            Core
-          </Link>
-        ) : (
-          <span className="px-2">Core</span>
+        {pathname !== '/' && (
+          <>
+            <Link href="/" className="px-2 text-black hover:underline">
+              Core
+            </Link>
+            <span>/</span>
+          </>
         )}
 
         {segments.map((segment, index) => {
           const href = '/' + segments.slice(0, index + 1).join('/');
           const isLast = index === segments.length - 1;
+          const label = formatSegment(segment);
 
           return (
             <React.Fragment key={href}>
-              <span>/</span>
+              {index > 0 && <span>/</span>}
               {isLast ? (
-                <span className="px-1 text-gray-700">{segment}</span>
+                <span className="px-1 text-gray-700">{label}</span>
               ) : (
                 <Link
                   href={href}
-                  className="px-1 text-blue-600 hover:underline"
+                  className="px-1 text-black hover:underline"
                 >
-                  {segment}
+                  {label}
                 </Link>
               )}
             </React.Fragment>
