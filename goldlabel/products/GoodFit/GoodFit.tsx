47d04a5cbc7a807e8252105afc5fe860ccf0507b
Goldlabel pr0 <github@goldlabel.pro>
2025-04-16 21:32:09 +0200

    initial States

--- goldlabel/products/GoodFit/GoodFit.tsx
+++ goldlabel/products/GoodFit/GoodFit.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import * as React from 'react';
-import { Box, Button, Grid, Card, CardHeader, CardContent, TextField, Typography } from '@mui/material';
+import { Box, Button, Grid, CardHeader, CardContent, TextField, Typography } from '@mui/material';
 import { Icon, VoiceRecorder } from '../../';
 
 export default function GoodFit() {
 
   const [jobDescription, setJobDescription] = React.useState('');
-
+  const showVoice = false
 
   return (
     <>
-      <Card sx={{ maxWidth: 800, margin: 'auto', mt: 4, p: 2 }}>
+      <Box sx={{ maxWidth: 800, margin: 'auto', mt: 4, p: 2 }}>
         <CardHeader
           avatar={<Icon icon="good-fit" color="primary" />}
           title="Good Fit?"
@@ -22,12 +22,12 @@ export default function GoodFit() {
           <Grid container spacing={2}>
 
             <Grid size={{xs: 12 }}>
-<Typography>
-This simple AI tool will tell you in seconds. Paste in a job description and our AI will judge if it's a good match. If it is, you’ll get a tailored CV you can download instantly. If not, all the best
-</Typography>
+            <Typography>
+              This simple AI tool will tell you in seconds. Paste in a job description and our AI will judge if it's a good match. If it is, you’ll get a tailored CV you can download instantly. If not, all the best
+            </Typography>
             </Grid>
 
-            <Grid size={{xs: 12, md: 6 }}>
+            <Grid size={{xs: 12, md: 8 }}>
               
               <TextField
                 sx={{background: 'white', mb: 2,}}
@@ -40,25 +40,23 @@ This simple AI tool will tell you in seconds. Paste in a job description and our
                 value={jobDescription}
                 onChange={(e) => setJobDescription(e.target.value)}
               />
-              <VoiceRecorder product="goodfit"/>
+              {showVoice ? <VoiceRecorder product="goodfit"/> : null }
+              
             </Grid>
 
-            <Grid size={{xs: 12, md: 6 }}>
+            <Grid size={{xs: 12, md: 4 }}>
               <Button
+                disabled
                 variant="contained"
                 onClick={() => {}}
               >
                 <Icon icon="openai" />
-                <Box sx={{ ml: 1, mr: 2 }}>Analyse This</Box>
-                
+                <Box sx={{ ml: 1, mr: 2 }}>Is it a good fit?</Box>
               </Button>
             </Grid>
-
           </Grid>
         </CardContent>
-
-      </Card>
-      
+      </Box>      
     </>
   );
 }
