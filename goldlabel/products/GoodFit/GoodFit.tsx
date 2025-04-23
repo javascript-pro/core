aa4d940bb9defbc6821c79b86f19ed50324cfcff
Goldlabel pr0 <github@goldlabel.pro>
2025-04-23 13:28:29 +0200

    100% Good Fit

--- goldlabel/products/GoodFit/GoodFit.tsx
+++ goldlabel/products/GoodFit/GoodFit.tsx
@@ -6,6 +6,9 @@ import {
   Grid,
   CardContent,
   TextField,
+  Alert,
+  AlertTitle,
+  Typography,
 } from '@mui/material';
 import { Icon } from '../../';
 import { useKey } from '../../cartridges/Uberedux';
@@ -22,7 +25,7 @@ export type Frontmatter = {
 };
 
 export type GoodFitProps = {
-  markdown: any;
+  markup: any;
 };
 
 export default function GoodFit() {
@@ -32,34 +35,56 @@ export default function GoodFit() {
     examples,
   } = goodfitState[0]
 
-  const [feedback, setFeedback] = React.useState('feedback');
-
-  const [jobDescription, setJobDescription] = React.useState('');
+  const [jd, setJD] = React.useState('');
   const [resume, setResume] = React.useState('');
   const [valid, setValid] = React.useState(false);
+  const [message, setMessage] = React.useState("Paste a Resume or JD");
+  const [error, setError] = React.useState(false);
 
   const validate = () => {
-    let valid = true
-    // let message = "All good"
-    // const validity =  {
-    //   valid,
-    //   message,
-    // };
-    console.warn('validate: valid', valid);
-    setValid(valid);
+    setTimeout(() => {
+      if (resume.length < 9 && jd.length < 9) {
+        setMessage("More detail needed");
+        setError(false);
+        setValid(false);
+        return;
+      }
+    
+      // if (resume.length < 9) {
+      //   setMessage("Resume needs 100 characters at least");
+      //   setError(true);
+      //   setValid(false);
+      //   return;
+      // }
+    
+      // if (jd.length < 9) {
+      //   setMessage("The JD needs 100 characters at least");
+      //   setError(true);
+      //   setValid(false);
+      //   return;
+      // }
+    
+      // If both are valid
+      setMessage("Analayse when ready");
+      setError(false);
+      setValid(true);
+    }, 200)
   };
-
+  
 
   const onUpdateFields = () => {
     validate()
   };
 
   const onPasteClick = (field: string) => {
-    console.warn('onPasteClick', field);
     if (field === "jd"){
-      setJobDescription(examples.jd)
+      setJD(examples.jd)
+      validate();
     }
-    if (field === "resume") setResume(examples.resume)
+    if (field === "resume") {
+      setResume(examples.resume);
+      validate();
+    }
   };
 
   const onAnalyse = () => {
@@ -72,26 +97,9 @@ export default function GoodFit() {
         {/* <CardHeader avatar={<Icon icon={'good-fit'} />} title={'Good fit?'} /> */}
 
         <CardContent>
+  
           <Grid container spacing={2}>
             <Grid size={{ xs: 12, md: 6 }}>
-
-              <Box sx={{ mb: 2 }}>
-                <Button 
-                  fullWidth
-                  // disabled={ valid ? false : true }
-                  // variant= { valid ? "contained" : "outlined" }
-                  variant= { "contained" }
-                  onClick={() => {
-                    onPasteClick("resume");
-                  }}>
-                  <Box sx={{ mt: 1 }}>
-                    <Icon icon={'down'} />
-                  </Box>
-                  <Box sx={{ mx: 1 }}>
-                    Paste Example Resume
-                  </Box>
-                </Button>
-              </Box>
 
               <TextField
                 sx={{
@@ -107,11 +115,48 @@ export default function GoodFit() {
                   onUpdateFields();
                 }}
               />
+
+              <Box sx={{ mt: 2 }}>
+                <Button 
+                  fullWidth
+                  // disabled={ valid ? false : true }
+                  // variant= { valid ? "contained" : "outlined" }
+                  variant= { "contained" }
+                  onClick={() => {
+                    onPasteClick("resume");
+                  }}>
+                  <Box sx={{ mt: 1 }}>
+                    <Icon icon={'up'} />
+                  </Box>
+                  <Box sx={{ mx: 1 }}>
+                    Example Resume
+                  </Box>
+                </Button>
+              </Box>
+
+
             </Grid>
 
             <Grid size={{ xs: 12, md: 6 }}>
 
-              <Box sx={{ mb: 2 }}>
+              
+
+              <TextField
+                sx={{
+                  background: "#F7F7F7",
+                }}
+                value={jd}
+                label="Job Description"
+                multiline
+                fullWidth
+                rows={7}
+                onChange={(e) => {
+                  setJD(e.target.value);
+                  onUpdateFields();
+                }}
+              />
+
+              <Box sx={{ mt: 2 }}>
                 <Button 
                   fullWidth
                   // disabled={ valid ? false : true }
@@ -121,31 +166,22 @@ export default function GoodFit() {
                     onPasteClick("jd");
                   }}>
                   <Box sx={{ mt: 1 }}>
-                    <Icon icon={'down'} />
+                    <Icon icon={'up'} />
                   </Box>
                   <Box sx={{ mx: 1 }}>
-                    Paste Example JD
+                    Example JD
                   </Box>
                 </Button>
               </Box>
 
-              <TextField
-                sx={{
-                  background: "#F7F7F7",
-                }}
-                value={jobDescription}
-                label="Job Description"
-                multiline
-                fullWidth
-                rows={7}
-                onChange={(e) => {
-                  setJobDescription(e.target.value);
-                  onUpdateFields();
-                }}
-              />
             </Grid>
 
             <Grid size={{ xs: 12}}>
+              <Alert severity={!error ? "info" : "warning"} 
+                sx={{mb: 2}}
+              >
+                <AlertTitle>{message}</AlertTitle>
+              </Alert>
               <Box sx={{ m: 0 }}>
                 <Button 
                   fullWidth
@@ -153,19 +189,15 @@ export default function GoodFit() {
                   variant= { valid ? "contained" : "outlined" }
                   onClick={onAnalyse}
                 >
-                  
                   <Box sx={{ mx: 1 }}>Analyse</Box>
                   <Box sx={{ mx: 1 }}>
                     <Icon icon={'right'} />
                   </Box>
                 </Button>
               </Box>
-
             </Grid>
-
           </Grid>
         </CardContent>
-
         {/* <pre>examples: {JSON.stringify(examples, null, 2)}</pre> */}
       </Box>
     </>
