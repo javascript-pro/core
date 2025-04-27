6667e0f453f173c4ce906739f2b41fcddc52a2e8
Goldlabel pr0 <github@goldlabel.pro>
2025-04-27 14:17:59 +0200

    50.4.7 gl-api

--- app/api/gl-api/good-fit/route.tsx
+++ app/api/gl-api/good-fit/route.tsx
@@ -1,4 +1,4 @@
-import config from '../config.json';
+import config from '../response.json';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
