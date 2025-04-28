c671ec51ec4d54a1a1700130ab3e3ceed480032d
Goldlabel pr0 <github@goldlabel.pro>
2025-04-28 15:22:54 +0200

    Refactors the catridges into gl-core

---
+++ gl-core/cartridges/Bouncer/initialState.tsx
@@ -0,0 +1,10 @@
+// import { UbereduxState } from '../../types';
+
+// export interface IBoucerState {}
+
+export const initialState: any = {
+  cartridge: 'bouncer',
+  modalOpen: true,
+  authed: false,
+  fingerprint: null,
+};
