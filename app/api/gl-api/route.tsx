7b292c4663556bf59523300d0d8bd2d5e15760e4
Chris Dorward <listingslab@gmail.com>
2025-05-21 13:51:22 +0200

    Updates GL API endpoint configuration
    Updates the GL API endpoint configuration to use the correct flickr album id.
    Removes unused code and comments.

--- app/api/gl-api/route.tsx
+++ app/api/gl-api/route.tsx
@@ -1,24 +1,19 @@
 // core/app/api/gl-api/route.ts
 import config from './config.json';
 import {
-  // NextRequest,
   NextResponse,
 } from 'next/server';
 
 export async function GET() {
-  // request: NextRequest
+  
   const { apibase } = config;
 
   return NextResponse.json({
     time: Date.now(),
     endpoint: `${apibase}/`,
-    // apibase,
     endpoints: [
       {
-        flickr: 'http://localhost:3000/api/gl-api/flickr/?album=7210000000',
-      },
-      {
-        openai: 'http://localhost:3000/api/gl-api/openai',
+        flickr: 'http://localhost:3000/api/gl-api/flickr/?album=72177720324245676',
       },
     ],
   });
