bfa2ce86b23e7f7b132c3263f0b5ab82cb86abc6
Chris Dorward <listingslab@gmail.com>
2025-05-25 19:22:22 +0200

    resetFlickr

--- gl-core/cartridges/Flickr/actions/resetFlickr.tsx
+++ gl-core/cartridges/Flickr/actions/resetFlickr.tsx
@@ -1,12 +1,12 @@
 import { TUbereduxDispatch } from '../../../';
 import { setUbereduxKey } from '../../../';
-import { FlickrinitialState } from '../';
+import { FlickrinitialState, initFlickr } from '../';
 
 export const resetFlickr =
   (): any => async (dispatch: TUbereduxDispatch, getState: () => any) => {
     try {
-      console.log('reseting Flickr');
       dispatch(setUbereduxKey({ key: 'flickr', value: FlickrinitialState }));
+      dispatch(initFlickr('72177720326317140'));
     } catch (e: unknown) {
       const msg = e instanceof Error ? e.message : String(e);
       dispatch(setUbereduxKey({ key: 'error', value: msg }));
