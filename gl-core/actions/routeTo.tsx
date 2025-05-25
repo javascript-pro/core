8b60a8b0b382e2ee5dc3a98f36092ec8283a70b8
Chris Dorward <listingslab@gmail.com>
2025-05-25 15:45:26 +0200

    Adds routeTo action

---
+++ gl-core/actions/routeTo.tsx
@@ -0,0 +1,27 @@
+// core/gl-core/actions/routeTo.tsx
+
+import { setUbereduxKey } from '../../gl-core';
+import { TUbereduxDispatch } from '../../gl-core';
+// import { store } from '../../gl-core/cartridges/Uberedux/store';
+
+export type TUbereduxState = {
+  // define shape if needed
+};
+
+export const routeTo =
+  (route: string, router?: any) => async (dispatch: TUbereduxDispatch) => {
+    try {
+      console.log('routeTo', route);
+    } catch (e: unknown) {
+      const errorMessage = e instanceof Error ? e.message : String(e);
+      dispatch(
+        setUbereduxKey({
+          key: 'error',
+          value: {
+            status: 'error',
+            errorMessage,
+          },
+        }),
+      );
+    }
+  };
