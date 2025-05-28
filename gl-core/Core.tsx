9acbd974c61b5f95982dde9d8b2ebfcd36dd575a
Chris Dorward <listingslab@gmail.com>
2025-05-28 12:22:08 +0200

    Improves layout and updates dependencies
    Refactors the core layout to improve responsiveness
    on mobile devices and adjust the sidebar appearance.
    
    Moves 'viewpoint' to the top of the initial state.
    Updates Flickr advert component.
    Renames prompts for clarity.
    Sets forceUpdate to true in config.

--- gl-core/Core.tsx
+++ gl-core/Core.tsx
@@ -1,6 +1,9 @@
 'use client';
 import * as React from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+import { 
+  usePathname, 
+  useRouter, 
+} from 'next/navigation';
 import { CssBaseline, Box, Grid } from '@mui/material';
 import {
   Theme,
@@ -37,12 +40,13 @@ export default function Core({
   const pathname = usePathname();
   const router = useRouter();
   const isMobile = useIsMobile();
-  const maxHeight = isMobile ? 150 : 180;
+  const maxHeight = isMobile ? 120 : 315;
   const isApp =
     pathname.startsWith('/cv') || pathname.startsWith('/free/flickr');
 
   const isCV = pathname === '/cv';
   const isFlickr = pathname === '/free/flickr';
+  
   let app = <></>;
   switch (true) {
     case pathname.startsWith('/cv'):
@@ -61,31 +65,8 @@ export default function Core({
       break;
   }
 
-  return (
-    <Theme>
-      <CssBaseline />
-      <Flash id="core">
-        <MovieClip id="content" opacity={0}>
-          <Header frontmatter={frontmatter} />
-
-          <Grid container spacing={1}>
-            <Grid
-              size={{
-                xs: 12,
-                sm: 6,
-                md: 8,
-                lg: 9,
-              }}
-            >
-              <Photo maxHeight={maxHeight} src={frontmatter?.image ?? null} />
-              <Box sx={{ p: 2 }}>
-                {pathname !== '/' && <PageBreadcrumb />}
-                <Box sx={{ height: 24 }} />
-                {isApp ? app : <RenderMarkdown>{body}</RenderMarkdown>}
-              </Box>
-            </Grid>
-
-            <Grid
+  const getAside = () => {
+    return  <Grid
               size={{
                 xs: 12,
                 sm: 6,
@@ -95,17 +76,17 @@ export default function Core({
             >
               <Box 
                 id="sidebar" 
-                component="aside" 
-                sx={{ mx: 0 }}
+                component="aside"
+                sx={{
+                  mr: 4,
+                  ml: isMobile ? 4 : 0,
+                }}
               >
-                { !isCV && <Box sx={{mx:4, mb: 2}}>
-                              <CV
-                                mode="advert" 
-                              />
+                { !isCV && <Box sx={{ mb: 2 }}>
+                              <CV mode="advert" />
                             </Box> }
                 { !isFlickr && <Flickr
                   mode="album-card"
-                  //  72157594233009954
                   id="72177720326317140"
                   onClick={() => {
                     router.push(`/free/flickr`);
@@ -113,9 +94,46 @@ export default function Core({
                 /> }
               </Box>
             </Grid>
+  };
+
+  return (
+    <Theme>
+      <CssBaseline />
+      <Flash id="core">
+        <MovieClip id="content" opacity={0}>
+          <Header frontmatter={frontmatter} />
+
+          <Grid container spacing={1}>
+
+            <Grid
+              size={{
+                xs: 12,
+                sm: 6,
+                md: 8,
+                lg: 9,
+              }}
+            >
+              <Photo 
+                maxHeight={maxHeight} 
+                src={frontmatter?.image ?? null}
+              />
+              { isMobile && getAside() }
+              <Box sx={{ px: 2 }}>
+                {pathname !== '/' && <PageBreadcrumb />}
+                <Box sx={{ height: 24 }} />
+                {isApp ? app : <RenderMarkdown>{body}</RenderMarkdown>}
+              </Box>
+            </Grid>
+            
+            { !isMobile && getAside() }
+            
           </Grid>
         </MovieClip>
       </Flash>
     </Theme>
   );
 }
+
+/*
+
+*/
