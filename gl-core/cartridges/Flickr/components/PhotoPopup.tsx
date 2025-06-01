d58ff00e989b5ffa9ca0157bec0a81f1e39dbef0
Chris Dorward <listingslab@gmail.com>
2025-06-01 22:20:41 +0200

    Adds photo popup functionality
    Enables users to view photos in a popup dialog.
    
    - Introduces a new `photoSelect` action to select and store a photo in the Uberedux store.
    - Implements a new `PhotoPopup` component to display the selected photo in a dialog.
    - Integrates the `PhotoPopup` component into the `Flickr` component.
    - Improves image size fetching to include thumbnails and other sizes.

--- gl-core/cartridges/Flickr/components/PhotoPopup.tsx
+++ gl-core/cartridges/Flickr/components/PhotoPopup.tsx
@@ -1,19 +1,111 @@
 'use client';
-
 import * as React from 'react';
-// import { Box, CardHeader, CardMedia } from '@mui/material';
-// import { MightyButton } from '../../../../gl-core';
+import {
+  Dialog,
+  DialogContent,
+  IconButton,
+  Typography,
+  Box,
+  CardHeader,
+  CardMedia,
+} from '@mui/material';
+import { useSlice, useDispatch, Icon } from '../../../../gl-core';
+import { photoSelect } from '../../Flickr';
 
-export type TPhotoPopup = {
-  [key: string]: any;
-};
+export default function PhotoPopup() {
+  const dispatch = useDispatch();
+  const photo = useSlice().flickr?.photo;
+  if (!photo) return null;
 
-export default function PhotoPopup({ photo = null }: TPhotoPopup) {
+  const title = photo.title || '';
+  const description = photo.description || '';
+  const src = photo?.sizes?.medium?.src || photo?.sizes?.small?.src;
+
+  const handleClose = () => dispatch(photoSelect(null));
+
+  if (!src) {
+    return (
+      <Dialog open onClose={handleClose} maxWidth="lg">
+        <CardHeader
+          title="Error"
+          action={
+            <IconButton onClick={handleClose}>
+              <Icon icon="close" />
+            </IconButton>
+          }
+        />
+        <DialogContent>
+          <Typography variant="body2" color="error">
+            No suitable image available.
+          </Typography>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   return (
-    <>
-      <pre style={{ fontSize: 10 }}>
-        photo: {JSON.stringify(photo, null, 2)}
-      </pre>
-    </>
+    <Dialog
+      open
+      onClose={handleClose}
+      PaperProps={{
+        sx: {
+          maxHeight: '90vh',
+          display: 'flex',
+          flexDirection: 'column',
+        },
+      }}
+    >
+      <CardHeader
+        title={title}
+        subheader={description}
+        avatar={
+          <IconButton 
+            onClick={() => window.open(photo.flickrUrl, '_blank')}
+          >
+            <Icon icon="flickr" />
+          </IconButton>
+        }
+        action={
+          <IconButton onClick={handleClose} color="inherit">
+            <Icon icon="close" />
+          </IconButton>
+        }
+      />
+      <DialogContent
+        sx={{
+          p: 2,
+          flex: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          overflow: 'hidden',
+        }}
+      >
+        <Box
+          sx={{
+            maxHeight: '100%',
+            maxWidth: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <CardMedia
+            component="img"
+            src={src}
+            alt={title}
+            sx={{
+              height: 'auto',
+              maxHeight: '100%',
+              width: 'auto',
+              maxWidth: '100%',
+              objectFit: 'contain',
+              borderRadius: 2,
+            }}
+          />
+        </Box>
+      </DialogContent>
+
+    </Dialog>
   );
 }
--- gl-core/cartridges/Flickr/index.tsx
+++ gl-core/cartridges/Flickr/index.tsx
@@ -1,15 +1,17 @@
 import Flickr from './Flickr';
 import { initialState as FlickrinitialState } from './initialState';
 import { initFlickr } from './actions/initFlickr';
+import { photoSelect } from './actions/photoSelect';
 import { AlbumCard, PhotoCard, PhotoPopup } from './components';
 import { resetFlickr } from './actions/resetFlickr';
 
 export {
+  FlickrinitialState,
   Flickr,
   PhotoPopup,
   resetFlickr,
   AlbumCard,
   PhotoCard,
   initFlickr,
-  FlickrinitialState,
+  photoSelect,
 };
