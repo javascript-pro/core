14701b488bbf9f5b1b23dd9f50524fb5d05888ad
Chris Dorward <listingslab@gmail.com>
2025-06-09 09:10:56 +0200

    Configures Fallmanager cartridge theme
    Sets up the Fallmanager cartridge with a dedicated theme configuration.
    
    This includes defining primary and secondary colors, background, text, and border styles to provide a consistent and visually appealing user experience.

--- gl-core/cartridges/Fallmanager/Fallmanager.tsx
+++ gl-core/cartridges/Fallmanager/Fallmanager.tsx
@@ -1,24 +1,19 @@
 'use client';
 // core/gl-core/cartridges/Fallmanager/Fallmanager.tsx
 import * as React from 'react';
+import config from './config.json';
 import { TFallmanager } from './types';
 // import { Box, Button, Typography } from '@mui/material';
-// import { useDispatch, navigateTo, Icon } from '../../../gl-core';
+import { Theme } from '../../../gl-core';
 import { Layout } from '../Fallmanager';
 
 export default function Fallmanager({ payload = null }: TFallmanager) {
-  // const dispatch = useDispatch();
-
-  // const handleCartridgeHome = () => {
-  //   console.log('Fallmanager payload', payload);
-  //   dispatch(navigateTo('/cartridges/new-cartridge'));
-  // };
 
   return (
-    <>
+    <Theme theme={config.theme as any}>
       <Layout>
         <>A bunch of chillen</>
       </Layout>
-    </>
+    </Theme>
   );
 }
