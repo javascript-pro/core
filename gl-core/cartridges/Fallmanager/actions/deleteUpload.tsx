008416298b5911e6dedad532531b12de0140dea5
Chris Dorward <listingslab@gmail.com>
2025-06-12 08:27:59 +0200

    Removes generic CRUD actions
    in favour of just creating individual actions for requirements

---
+++ gl-core/cartridges/Fallmanager/actions/deleteUpload.tsx
@@ -0,0 +1,20 @@
+// core/gl-core/cartridges/Fallmanager/actions/deleteUpload.tsx
+import { TUbereduxDispatch } from '../../../../gl-core/types';
+import { setUbereduxKey } from '../../../../gl-core';
+
+export type TUpload = {
+    id: string;
+}
+
+export const deleteUpload =
+  (
+    id = 'notset'
+): any =>
+  async (dispatch: TUbereduxDispatch, getState: () => any) => {
+    try {
+      console.log('deleteUpload', id);
+    } catch (e: unknown) {
+      const msg = e instanceof Error ? e.message : String(e);
+      dispatch(setUbereduxKey({ key: 'error', value: msg }));
+    }
+  };
