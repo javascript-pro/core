2100f07d97c7bab0cdc807c9b40a0a0b2a6007ab
Chris Dorward <listingslab@gmail.com>
2025-07-09 12:59:45 +0200

    Removes phone number from CV download
    Removes the phone number link from the CV download component.
    
    This simplifies the contact information displayed and maintains a cleaner presentation.

--- gl-core/cartridges/CV/components/Download.tsx
+++ gl-core/cartridges/CV/components/Download.tsx
@@ -15,8 +15,7 @@ export default function Download(cv: any) {
       <h2>Chris Dorward</h2>
       <h4>
         <a href="https://goldlabel.pro/cv">goldlabel.pro</a> | 
-        <a href="mailto:goldlabel.apps@gmail.com">goldlabel.apps@gmail.com</a> | 
-        <a href="https://wa.me/447745763122">+44 07745763122</a>
+        <a href="mailto:goldlabel.apps@gmail.com">goldlabel.apps@gmail.com</a>
       </h4>
     ${marked.parse(cv.cv || '') as string}`;
     const fullHTML = templatePDF(html);
